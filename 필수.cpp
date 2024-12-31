dee370cf334a7a5c2b677ea71609f57553f5b55d
nyamnyumnyeom <jincar000@naver.com>
2024-12-31 15:56:55 +0900

    필수 update

---
+++ 필수.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include<vector>
+
+
+template <typename T>
+
+class SimpleVector {
+private:
+    T* data;
+    int arsize;
+    int max;
+
+public:
+
+    SimpleVector() : arsize(0), max(10) {
+        data = new T[max];
+    }
+
+
+    SimpleVector(int capacity) : arsize(0), max(capacity) {
+        data = new T[max];
+    }
+
+
+    ~SimpleVector() {
+        delete[] data;
+    }
+
+
+    void push_back(const T& value) {
+        if (arsize < max) {
+            data[arsize] = value;
+            ++arsize;
+        }
+    }
+
+
+    void pop_back() {
+        if (arsize > 0) {
+            --arsize;
+        }
+    }
+
+
+    int size() const {
+        return arsize;
+    }
+
+
+    int capacity() const {
+        return max;
+    }
+};
+
+int main() {
+    SimpleVector<int> vec1;
+    SimpleVector<double> vec2(5);
+
+    return 0;
+}
